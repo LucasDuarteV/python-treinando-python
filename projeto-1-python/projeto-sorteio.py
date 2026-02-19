f05906475c549a319b056aa9f94435c271e8ca54
Lucas Duarte V <lucasdv2733@gmail.com>
2026-02-19 14:00:53 -0300

    criando um programa para realizar sorteios por quantidade de participantes

---
+++ projeto-1-python/projeto-sorteio.py
@@ -0,0 +1,7 @@
+import random 
+
+num_participantes = int(input('Digite a quantidade de participantes: '))
+
+resultado = random.randint(1,num_participantes)
+
+print(f'O número sorteado é {resultado}')
