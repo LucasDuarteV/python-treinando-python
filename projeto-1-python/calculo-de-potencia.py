0128207ecea97a48e4a9d32b2b47f806d1c0936e
Lucas Duarte V <lucasdv2733@gmail.com>
2026-02-19 13:42:24 -0300

    programa para calcular a potencia

---
+++ projeto-1-python/calculo-de-potencia.py
@@ -0,0 +1,9 @@
+import math
+
+base = int(input('Digite a base: '))
+elevado = int(input('Digite o valor elevado: '))
+
+resultado = int(math.pow(base,elevado))
+
+print(f' O resultado de {base} elevado a {elevado} é igual á: {resultado}')
+
