2a8c6432ced8d187ba2481b5085d6615dd2216cb
Lucas Duarte V <lucasdv2733@gmail.com>
2026-02-23 16:13:48 -0300

    funcoes usando map

---
+++ projeto-1-python/funcoes-map.py
@@ -0,0 +1,11 @@
+notas = [9,4,8,9]
+
+qualitativo = 0.5
+
+notas_atualizadas = map(lambda x: x + qualitativo,notas)
+
+notas_atualizadas = list(notas_atualizadas)
+
+print(notas_atualizadas)
+
+print(notas)
