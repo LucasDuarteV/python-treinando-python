4d273be7ccba5272b4bb1bc09c62b9bc17f1178b
Lucas Duarte V <lucasdv2733@gmail.com>
2026-02-23 16:24:37 -0300

    desafio alura código lendo lista

---
+++ projeto-1-python/programa-lista.py
@@ -0,0 +1,11 @@
+lista = [16, 14, 63, 65, 17, 99, 70, 11, 20, 48, 79, 32, 17, 89, 12, 25, 66]
+
+tamanho_lista = len(lista)
+
+menor = min(lista)
+
+maior = max(lista)
+
+soma = sum(lista)
+
+print(f'A lista possui {tamanho_lista} números em que o maior número é {maior} e o menor número é {menor}. A soma dos valores presentes nela é igual a {soma}')
