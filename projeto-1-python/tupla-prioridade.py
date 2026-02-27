c1af9845dae68dfc4f82e3b43e2e04e6f35bbac9
Lucas Duarte V <lucasdv2733@gmail.com>
2026-02-27 10:29:46 -0300

    numerando uma lista

---
+++ projeto-1-python/tupla-prioridade.py
@@ -0,0 +1,9 @@
+lista = ['Pedro', 'Júlia', 'Otávio', 'Eduardo']
+
+lista_de_tuplas = []
+
+for i,nome in enumerate(lista):
+    lista_de_tuplas.append((i,nome))
+    
+    
+print(f'{lista_de_tuplas}')
