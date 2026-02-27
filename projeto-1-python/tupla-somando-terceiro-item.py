9c6bf8d0b82a4adae9483392a5eacf6ef2f64ad5
Lucas Duarte V <lucasdv2733@gmail.com>
2026-02-26 21:31:53 -0300

    buscando o terceiro elemento da lista

---
+++ projeto-1-python/tupla-somando-terceiro-item.py
@@ -0,0 +1,14 @@
+lista_de_tuplas = [('Pedro', 1.74, 81), ('Júlia', 1.65, 67), ('Otávio', 1.81, 83)]
+
+peso = []
+
+for tupla in lista_de_tuplas:
+    
+    terceiro_elemento = tupla[2]
+    
+    peso.append(terceiro_elemento)
+    
+print(f'{peso}')
+    
+
+    
