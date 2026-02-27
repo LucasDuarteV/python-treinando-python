3da170509a09de09988dc46f348975e856005e1f
Lucas Duarte V <lucasdv2733@gmail.com>
2026-02-26 21:24:25 -0300

    somando a lista

---
+++ projeto-1-python/soma-de-cada-item-lista.py
@@ -0,0 +1,6 @@
+lista_de_listas = [[4,6,5,9], [1,0,7,2], [3,4,1,8]]
+
+
+for lista_interna in lista_de_listas:
+    soma = sum(lista_interna)
+    print(f'A Lista é {lista_interna} a soma dos itens dessa lista é igual: {soma}')
